47803df5ed2f77a1d77ed056eab78dc048650fd7
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2024-08-15 16:28:43 +0300

    fix sidewinder algorithm

--- lib/algorithms/maze/sidewinder.ts
+++ lib/algorithms/maze/sidewinder.ts
@@ -24,15 +24,17 @@ export const Sidewinder = async (
   }
 
   for (let row = 1; row < MAX_ROWS; row += 2) {
-    for (let col = 1; col < MAX_COLS-1; col+=2) {
+    for (let col = 1; col < MAX_COLS - 1; col += 2) {
       if (isEqualRowCol(endTile, row, col)) {
         await destroyWall(grid, row, col, "top", speed);
-        await destroyWall(grid, row, col, "left", speed);
         continue;
-      } else if (row === 1 && col !== MAX_COLS-2) {
+      } else if (row === 1) {
+        if (col === MAX_COLS - 2) {
+          continue;
+        }
         await destroyWall(grid, row, col, "right", speed);
       } else {
-        let rndRightCol = getRndNum(col, MAX_COLS-1);
+        let rndRightCol = getRndNum(col, MAX_COLS - 1);
         let topRndCol = getRndNum(col, rndRightCol);
 
         if (rndRightCol % 2 === 0) {
